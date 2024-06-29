0bde01b80b8e9d94e2ec626a3472891d60fad203
Julian <rijkenjulian@gmail.com>
2024-06-29 22:47:45 +0200

    chore: Using string_view for game settings

--- src/Engine/GameSettings.h
+++ src/Engine/GameSettings.h
@@ -2,7 +2,8 @@
 #ifndef GAMESETTINGS_H
 #define GAMESETTINGS_H
 
-#include <string>
+
+#include <string_view>
 
 namespace jul
 {
@@ -11,7 +12,7 @@ namespace jul
     public:
         static float GetAspectRatio() { return static_cast<float>(g_WindowWidth) / static_cast<float>(g_WindowHeight); }
 
-        inline static std::string g_WindowTitle = "Julgen - Custom Engine by Julian Rijken";
+        inline static std::string_view g_WindowTitle = "Julgen - Custom Engine by Julian Rijken";
         inline static int g_RenderWidth{ 1280 };
         inline static int g_RenderHeight{ 720 };
         inline static int g_WindowWidth{ 1280 };
